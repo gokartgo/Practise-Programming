fa47d88690c459772d10e02b6b6a18fef912a01d
gokartgo <gokart.007@hotmail.com>
2020-05-03 12:24:31 +0700

    add test pointer

--- algorithm/test.c
+++ algorithm/test.c
@@ -1,8 +1,23 @@
 #include<stdio.h>
+
+void test(int** d) {
+  printf("%p\n",d);
+  printf("a%d\n",**d);
+}
+
 int main() {
-    int *a = 0,**c;
+    int *a,**c, *d;
     int b = 5;
     a = &b;
-    c = &a;
-    printf("%p %p %p",&b,a,*c);
+    printf("%p\n",&a);
+    *a = 6;
+    printf("%d\n",*a);
+    test(&a);
+    printf("%p\n",&a);
+    printf("%d",*a);
+    // int *a = 0,**c;
+    // int b = 5;
+    // a = &b;
+    // c = &a;
+    // printf("%p %p %p",&b,a,*c);
 }
