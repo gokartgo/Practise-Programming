29ee905cb12e4656732ae9c24866a6be1a43f4af
gokartgo <gokart.007@hotmail.com>
2020-05-17 17:24:54 +0700

    hackerrank finish find the median

---
+++ Hackerrank/Practice/easy/find_the_median.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the findMedian function below.
+function findMedian(arr) {
+    arr.sort((a,b) => a-b)
+    if(arr.length % 2 === 0) {
+        return (arr[arr.length/2] + arr[arr.length/2 - 1])/2
+    } else {
+        return arr[Math.ceil(arr.length/2) - 1]
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = findMedian(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
