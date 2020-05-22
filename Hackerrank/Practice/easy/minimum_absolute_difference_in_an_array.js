b5f086b0c2aecd51a7d23907f4ecfc71b1dab5e2
gokartgo <gokart.007@hotmail.com>
2020-05-22 23:54:06 +0700

    not finish hackerrank minimum absolute difference

---
+++ Hackerrank/Practice/easy/minimum_absolute_difference_in_an_array.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumAbsoluteDifference function below.
+function minimumAbsoluteDifference(arr) {
+    let min = 1000000000
+    for(let i = 0;i<arr.length - 1;i++) {
+        for(let j = i+1;j<arr.length;j++) {
+            if(min > Math.abs(arr[i] - arr[j])) {
+                min = Math.abs(arr[i] - arr[j])
+            }
+        }
+    }
+    return min
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = minimumAbsoluteDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
