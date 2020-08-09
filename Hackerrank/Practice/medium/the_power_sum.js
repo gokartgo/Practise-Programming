a273130e70957329da42cd6c0065145313fa5035
gokartgo <gokart.007@hotmail.com>
2020-08-09 14:17:40 +0700

    hackerrank the power sum not complete

---
+++ Hackerrank/Practice/medium/the_power_sum.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+let number = []
+let count = []
+let numberCombination = []
+let answer = 0
+let exponent = 1
+let check
+
+function checkAnswer(number) {
+    let sum = 0
+    for(let i=0;i<number.length;i++) {
+        sum+= Math.pow(number[i],exponent)
+    }
+    if(sum === check) {
+        answer++
+    }
+}
+
+function combination(start,number,count,level,numberCombination) {
+    for(let i=start;i<number.length;i++) {
+        if(count[i] === 0) {
+            continue
+        }
+        count[i]--
+        numberCombination[level] = number[i]
+        let cloneNumberCombination = [...numberCombination]
+        checkAnswer(cloneNumberCombination)
+        combination(i,number,count,level+1,cloneNumberCombination)
+        count[i]++
+    }
+}
+
+// Complete the powerSum function below.
+function powerSum(X, N) {
+    let i
+    answer = 0
+    exponent = N
+    check = X
+    for(i=1;i<=X;i++) {
+        if(Math.pow(i,N) > X) {
+            break
+        }
+    }
+    for(let j=0;j<i-1;j++) {
+        number[j] = j+1
+        count[j] = 1
+    }
+    combination(0,number,count,0,[])
+    return answer
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const X = parseInt(readLine(), 10);
+
+    const N = parseInt(readLine(), 10);
+
+    let result = powerSum(X, N);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
