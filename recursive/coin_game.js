cfb7eecf646de22700b4e22288b9b68274137f0f
gokartgo <gokart.007@hotmail.com>
2020-08-25 21:48:04 +0700

    try to change mod to recursive

--- recursive/coin_game.js
+++ recursive/coin_game.js
@@ -1,3 +1,24 @@
+function is_player_first_winner(coin) {
+  if(coin === 0 || coin === 1 || coin === 2 || coin === 4) {
+    return true
+  }
+  if(coin === 3) {
+    return false
+  }
+  
+  if(
+    is_player_first_winner(coin - 1) &&
+    is_player_first_winner(coin - 2) &&
+    is_player_first_winner(coin - 4)
+  ) {
+    return false
+  } else {
+    return true
+  }
+}
+
+// console.log(is_player_first_winner(8))
+
 function coin_game(coin, level) {
   let sum = 0
 
@@ -11,26 +32,26 @@ function coin_game(coin, level) {
     return 0
   }
 
-  const one = (coin - 1) % 3 === 0
-  const two = (coin - 2) % 3 === 0
-  const four = (coin - 4) % 3 === 0
+  const one = is_player_first_winner(coin - 1)
+  const two = is_player_first_winner(coin - 2)
+  const four = is_player_first_winner(coin - 4)
 
   if(!one && !two && !four) {
-    return coin_game1(coin - 1, level + 1) +
-    coin_game1(coin - 2, level + 1) +
-    coin_game1(coin - 4, level + 1)
+    return coin_game(coin - 1, level + 1) +
+    coin_game(coin - 2, level + 1) +
+    coin_game(coin - 4, level + 1)
   }
   if(one) {
-    sum += coin_game1(coin - 1, level + 1)
+    sum += coin_game(coin - 1, level + 1)
   }
   if(two) {
-    sum += coin_game1(coin - 2, level + 1)
+    sum += coin_game(coin - 2, level + 1)
   }
   if(four) {
-    sum += coin_game1(coin - 4, level + 1)
+    sum += coin_game(coin - 4, level + 1)
   }
 
   return sum
 }
 
-console.log(coin_game1(30, 0))
+console.log(coin_game(10, 0))
