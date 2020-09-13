3fc2b32e945af81441e7285a559ee24b10ac4d18
gokartgo <gokart.007@hotmail.com>
2020-09-13 15:09:22 +0700

    pass climbing the leaderboard

--- Hackerrank/Practice/medium/climbing_the_leaderboard.js
+++ Hackerrank/Practice/medium/climbing_the_leaderboard.js
@@ -26,39 +26,46 @@ function readLine() {
 
 // Complete the climbingLeaderboard function below.
 function climbingLeaderboard(scores, alice) {
-    let score = scores.sort((a,b) => a - b),index = 1, score_object = {}
-    
-    for(let j=score.length - 1;j>=0;j--) {
-        if(!score_object[`${score[j]}`]) {
-            score_object[`${score[j]}`] = index++
+    let answer = []
+    for (let i = 0; i < alice.length; i++) {
+        let find = true
+        let max = 0
+        let min = scores.length - 1
+        if (scores[0] <= alice[i]) {
+            answer[i] = 1
+            continue
+        } else if (scores[min] == alice[i]) {
+            answer[i] = min + 1
+            continue
+        } else if (scores[min] > alice[i]) {
+            answer[i] = min + 2
+            continue
+        }
+        let index = Math.ceil((min + max) / 2)
+        while (find) {
+            if (alice[i] < scores[index] && alice[i] > scores[index + 1]) {
+                answer[i] = index + 2
+                find = false
+            } else if (
+                (alice[i] > scores[index] && alice[i] < scores[index - 1]) ||
+                (alice[i] === scores[index])
+            ) {
+                answer[i] = index + 1
+                find = false
+            } else if (alice[i] > scores[index]) {
+                min = index + 1
+                index = Math.floor((min + max) / 2)
+            } else if (alice[i] < scores[index]) {
+                max = index - 1
+                index = Math.ceil((min + max) / 2)
+            } else {
+                answer[i] = index + 1
+                find = false
+            }
         }
     }
 
-    for(let i=0;i<alice.length;i++) {
-        rank = 1
-        scores_check = {}
-        for(let j=0;j<scores.length;j++) {
-            if(alice[i] < scores[j] && !scores_check[`${scores[j]}`]) {
-                rank++
-                scores_check[`${scores[j]}`] = true
-            }
-        }
-        answer[index++] = rank
-    }
     return answer
-    // let scores_check = {}, rank = 1, answer = [], index = 0
-    // for(let i=0;i<alice.length;i++) {
-    //     rank = 1
-    //     scores_check = {}
-    //     for(let j=0;j<scores.length;j++) {
-    //         if(alice[i] < scores[j] && !scores_check[`${scores[j]}`]) {
-    //             rank++
-    //             scores_check[`${scores[j]}`] = true
-    //         }
-    //     }
-    //     answer[index++] = rank
-    // }
-    // return answer
 }
 
 function main() {
@@ -72,7 +79,15 @@ function main() {
 
     const alice = readLine().split(' ').map(aliceTemp => parseInt(aliceTemp, 10));
 
-    let result = climbingLeaderboard(scores, alice);
+    let new_scores = []
+    let index_new_scores = 0
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] !== scores[i + 1]) {
+            new_scores[index_new_scores++] = scores[i]
+        }
+    }
+
+    let result = climbingLeaderboard(new_scores, alice);
 
     ws.write(result.join("\n") + "\n");
 
