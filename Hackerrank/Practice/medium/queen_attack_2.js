91afe16ed298aaa9bd7cac0b2a20419da1acc1b7
gokartgo <gokart.007@hotmail.com>
2020-09-20 13:09:07 +0700

    finish queen attack 2 hackerrank

--- Hackerrank/Practice/medium/queen_attack_2.js
+++ Hackerrank/Practice/medium/queen_attack_2.js
@@ -27,7 +27,7 @@ function readLine() {
 // Complete the queensAttack function below.
 // n is size board
 function queensAttack(n, k, r_q, c_q, obstacles) {
-    let answer = []
+    let answer = [], sum = 0
     answer[0] = c_q - 1 // <-
     answer[1] = n - c_q // ->
     answer[2] = n - r_q // |^
@@ -36,26 +36,42 @@ function queensAttack(n, k, r_q, c_q, obstacles) {
     answer[5] = r_q >= c_q ? c_q - 1 : r_q - 1 // /v
     answer[6] = n - r_q >= n - c_q ? n - c_q : n - r_q // /^
     answer[7] = r_q - 1 >= n - c_q ? n - c_q : r_q - 1 // \v
-    console.log(answer)
-    // for(let i = 0;i<obstacles.length;i++) {
-    //     if( r_q === obstacles[i][0] && c_q > obstacles[i][1] ) { // <-
-            
-    //     } else if ( r_q === obstacles[i][0] && c_q < obstacles[i][1] ) { // ->
 
-    //     } else if ( r_q < obstacles[i][0] && c_q === obstacles[i][1] ) { // |^
-            
-    //     } else if ( r_q > obstacles[i][0] && c_q === obstacles[i][1] ) { // |v
-            
-    //     } else if ( r_q < obstacles[i][0] && c_q > obstacles[i][1] ) { // \^
-            
-    //     } else if ( r_q > obstacles[i][0] && c_q > obstacles[i][1] ) { // /v
-            
-    //     } else if ( r_q < obstacles[i][0] && c_q < obstacles[i][1] ) { // /^
-            
-    //     } else if ( r_q > obstacles[i][0] && c_q < obstacles[i][1] ) { // \v
-            
-    //     }
-    // }
+    for(let i = 0;i<obstacles.length;i++) {
+        if( r_q === obstacles[i][0] && c_q > obstacles[i][1] ) { // <-
+            answer[0] = c_q - obstacles[i][1] - 1 < answer[0] ? 
+            c_q - obstacles[i][1] - 1 : answer[0]
+        } else if ( r_q === obstacles[i][0] && c_q < obstacles[i][1] ) { // ->
+            answer[1] = obstacles[i][1] - c_q - 1 < answer[1] ?
+            obstacles[i][1] - c_q - 1 : answer[1]
+        } else if ( r_q < obstacles[i][0] && c_q === obstacles[i][1] ) { // |^
+            answer[2] = obstacles[i][0] - r_q - 1 < answer[2] ?
+            obstacles[i][0] - r_q - 1 : answer[2]
+        } else if ( r_q > obstacles[i][0] && c_q === obstacles[i][1] ) { // |v
+            answer[3] = r_q - obstacles[i][0] - 1 < answer[3] ?
+            r_q - obstacles[i][0] - 1 : answer[3]
+        } else if (Math.abs(r_q - obstacles[i][0]) === Math.abs(c_q - obstacles[i][1])) {
+            if ( r_q < obstacles[i][0] && c_q > obstacles[i][1] ) { // \^
+                answer[4] = obstacles[i][0] - r_q - 1 < answer[4] ?
+                obstacles[i][0] - r_q - 1 : answer[4]
+            } else if ( r_q > obstacles[i][0] && c_q > obstacles[i][1] ) { // /v
+                answer[5] = r_q - obstacles[i][0] - 1 < answer[5] ?
+                r_q - obstacles[i][0] - 1 : answer[5]
+            } else if ( r_q < obstacles[i][0] && c_q < obstacles[i][1] ) { // /^
+                answer[6] = obstacles[i][0] - r_q - 1 < answer[6] ?
+                obstacles[i][0] - r_q - 1 : answer[6]
+            } else if ( r_q > obstacles[i][0] && c_q < obstacles[i][1] ) { // \v
+                answer[7] = r_q - obstacles[i][0] - 1 < answer[7] ?
+                r_q - obstacles[i][0] - 1 : answer[7]
+            }
+        }
+    }
+
+    for(let i = 0;i<answer.length;i++) {
+        sum += answer[i]
+    }
+    
+    return sum
 }
 
 function main() {
