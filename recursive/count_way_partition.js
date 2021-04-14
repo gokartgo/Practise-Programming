cde718e21dea5b2d1a092980c9f1dfe7559ff587
gokartgo <gokart.007@hotmail.com>
2021-04-14 14:41:21 +0700

    refactor count way partition

--- recursive/count_way_partition.js
+++ recursive/count_way_partition.js
@@ -5,16 +5,13 @@
 // * + * + * + *
 
 function count_way_partition(n, m) {
-    if (n === 0 || m === 1) {
+    if (n === 0) {
         return 1
     }
-    if (m === 0) {
+    if (n < 0 || m === 0) {
         return 0
-    }
-    if (m > n) {
-        m = n
     }
     return count_way_partition(n, m - 1) + count_way_partition(n - m, m)
 }
 
-console.log(count_way_partition(5, 2))
+console.log(count_way_partition(26, 26))
