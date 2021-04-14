8a20a11afe2618b4ba555d4673eaacd2a2809982
gokartgo <gokart.007@hotmail.com>
2021-04-14 15:30:00 +0700

    review about tower of hanoi

--- recursive/tower_of_hanoi.js
+++ recursive/tower_of_hanoi.js
@@ -1,3 +1,12 @@
+// function tower_of_hanoi(s, a, d, level) {
+//   if(level === 0) {
+//     return
+//   }
+//   tower_of_hanoi(s, d, a, level - 1)
+//   console.log(`${s} -> ${d}`)
+//   tower_of_hanoi(a, s, d, level - 1)
+// }
+
 function tower_of_hanoi(n, source, auxiliary, dest) {
     if (n === 1) {
         console.log(source, '------>', dest)
