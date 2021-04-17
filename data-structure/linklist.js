29f56efb08524572baeba24a6197c722ab655fd3
gokartgo <gokart.007@hotmail.com>
2021-04-17 22:30:59 +0700

    prepare linkedlist js

---
+++ data-structure/linklist.js
@@ -0,0 +1,22 @@
+class Node {
+  constructor(data, next) {
+    this.data = data
+    this.next = next
+  }
+}
+
+class LinkedList {
+  // Insert first Node
+
+  // Insert last Node
+
+  // Insert at index
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear List
+
+  // Print list data
+}
